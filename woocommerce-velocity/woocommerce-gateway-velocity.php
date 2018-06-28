4a5f432188ae1ab6482129e631a72465a59a2509
ahamoudi <ahamoudi@nabancard.com>
2018-06-28 15:02:35 -0600

    Update Card EXP date list
    Update Card EXP date list

--- woocommerce-velocity/woocommerce-gateway-velocity.php
+++ woocommerce-velocity/woocommerce-gateway-velocity.php
@@ -306,14 +306,24 @@ function woocommerce_velocity_init() {
                     <option value="12">Dec</option>
                 </select>
                 <select id="cc-exp-year" class="txt" name="exp_year">
-                    <option value="15">2015</option>
-                    <option value="16">2016</option>
-                    <option value="17">2017</option>
                     <option value="18">2018</option>
                     <option value="19">2019</option>
                     <option value="20">2020</option>
                     <option value="21">2021</option>
                     <option value="22">2022</option>
+					<option value="23">2023</option>
+					<option value="24">2024</option>
+					<option value="25">2025</option>
+					<option value="26">2026</option>
+					<option value="27">2027</option>
+					<option value="28">2028</option>
+					<option value="29">2029</option>
+					<option value="30">2030</option>
+					<option value="31">2031</option>
+					<option value="32">2032</option>
+					<option value="33">2033</option>
+					<option value="34">2034</option>
+					<option value="35">2035</option>
                 </select>
             </div>
 
